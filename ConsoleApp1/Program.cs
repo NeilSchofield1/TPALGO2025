bc7233c69b1f55171059373e1a9ebf0565dc98cc
Connor Dompierre Simard <2553624@cstjean.qc.ca>
2025-11-28 11:02:45 -0500

    Ajoutez des fichiers projet.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsoleApp1 {
+
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+
+    
+
